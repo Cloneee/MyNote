a80673c096db9f0a29e3971f353bfd8228ee735d
nguyenvnbrtd <54762339+nguyenvnbrtd@users.noreply.github.com>
2022-03-14 23:00:41 +0700

    create splash screen, login screen

--- app/src/main/java/com/example/mynote/MainActivity.java
+++ app/src/main/java/com/example/mynote/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.mynote;
 
-import androidx.appcompat.app.AppCompatActivity;
-import com.example.mynote.databinding.ActivityMainBinding;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mynote.databinding.ActivityMainBinding;
+import com.example.mynote.screens.LoginScreen;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+        startActivity(new Intent(this, LoginScreen.class));
     }
 }
