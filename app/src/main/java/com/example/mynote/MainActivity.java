aaff34e6ec4ff71f69290fa8a84190a99fcd4dad
nguyenvnbrtd <54762339+nguyenvnbrtd@users.noreply.github.com>
2022-03-15 16:44:32 +0700

    create register, home screen

--- app/src/main/java/com/example/mynote/MainActivity.java
+++ app/src/main/java/com/example/mynote/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.mynote;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mynote.databinding.ActivityMainBinding;
-import com.example.mynote.screens.LoginScreen;
+import com.example.mynote.screens.login.LoginScreen;
 
 public class MainActivity extends AppCompatActivity {
 
